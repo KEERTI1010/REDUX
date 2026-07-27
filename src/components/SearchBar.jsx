57baf2eb161425d84fefe8f0a38fe42279a474a7
KEERTI1010 <shetkeerti6@gmail.com>
2026-07-27 23:06:55 +0530

    REDUX

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,12 +4,16 @@ const SearchBar = () => {
 
     const [text, settext] = useState('')
 
+    const submitHandler = () => {
+      console.log("Form Submitted");
+    }
+
   return (
     <div>
-        <form>
-            <input className = "border-white border-2 text-center m-4 p-2 rounded-b-md px-4 text-xl text-white " type="text"
+        <form onSubmit={submitHandler} className='flex bg-blue-950 gap-5 p-10'>
+            <input required className = "w-full border-white border-2 m-4 p-2 rounded-b-md px-4 text-xl text-white " type="text"
              placeholder = 'Search Anything....'  />
-            <button className = "border-white border-2 text-center m-4 p-2 rounded-b-md px-4 text-xl text-white ">Search</button>
+            <button className = "active:scale-95 cursor-pointer border-white border-2 m-4 p-2 rounded-b-md px-4 text-xl text-white ">Search</button>
         </form>
     </div>
   )
