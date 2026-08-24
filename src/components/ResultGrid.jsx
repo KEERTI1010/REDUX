f0d1e211bcb2b7cf233ebd5bff975e8a44c7e6e7
KEERTI1010 <shetkeerti6@gmail.com>
2026-08-24 11:43:47 +0530

    REDUX

--- src/components/ResultGrid.jsx
+++ src/components/ResultGrid.jsx
@@ -8,6 +8,7 @@ const ResultGrid = () => {
     const {query,activeTab,results,loading,error} = useSelector((store)=> store.search)
 
     useEffect(function (){
+        if (!query.trim()) return
 
         const getData = async ()=>{
 
@@ -26,18 +27,26 @@ const ResultGrid = () => {
                 let response = await fetchVideos(query)
                 data = response.videos.map((item)=>({
                     id:item.id,
-                    type:'photo',
-                    title:item.alt_description,
-                    thumbnail:item.urls.small,
-                    src:item.urls.full
+                    type:'video',
+                    title:item.user.name || 'video',
+                    thumbnail:item.image,
+                    src:item.video_files[0].link
                 }))
             }
             if(activeTab == "gif"){
                 let response = await fetchGIF(query)
-                data = response.data;
+                data = response.data.map((item)=>({
+                    id:item.id,
+                    type:'gif',
+                    title:item.title || 'GIF',
+                    thumbnail:item.images.fixed_height_small.url,
+                    src:item.images.original.url
+                }))
             }
             console.log(data);
     }
+
+        if (!query.trim()) return
 
         getData()
     }, [query,activeTab])
